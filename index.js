2a46fdbdaa65c348248738494b15a927fd457028
Raúl Rafael Carvajal Gutiérrez <Ral Rafael Carvajal Gutirrez>
2019-02-26 09:56:26 -0700

    modificaciones

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ fs.readFile('list.csv','utf8',(err, data) => {
     line.shift();
     line.forEach((reg)=>{
         var aux=reg.split(",")
-        console.log(`#${aux[0]} - Número de control: ${aux[1]} - Nombre: ${aux[2]} - Calificación: ${aux[3]}`);
+        console.log(`#${aux[0]}\nNúmero de control: ${aux[1]}\nNombre: ${aux[2]}\nCalificación: ${aux[3]}\n-----------------------------------------`);
     })
 });
